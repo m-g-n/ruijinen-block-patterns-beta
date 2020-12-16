0b4e2df75b9c3a4b31a9f8625855cbf132dfd03d
kasumi <kasumi@megane.in>
2020-12-16 16:35:49 +0900

    RUIJINEN-89 ブロックパターン作成にあたり必要なファイルやディレクトリーをプラグイン内に整備する
    定数の宣言を追加。

--- rui-jin-en-pattern.php
+++ rui-jin-en-pattern.php
@@ -9,6 +9,12 @@
  * @license GPL-2.0+
  */
 
+ /**
+ * 定数を宣言
+ */
+define( 'RJE_PLUGIN_URL', plugins_url( '', __FILE__ ) ); // このプラグインのURL
+define( 'RJE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) ); // このプラグインのパス
+
 /**
  * Snow Monkey 以外のテーマを利用している場合は有効化してもカスタマイズが反映されないようにする
  */
