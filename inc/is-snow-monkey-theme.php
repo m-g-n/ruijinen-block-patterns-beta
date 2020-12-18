a6b8564cd777d72e5bb15ba3fee57a9bec1ea392
lunaluna <web@lunalunadesign.com>
2020-12-18 17:11:58 +0900

    テーマ Snow Monkey が有効化されていないときに管理画面にアラートを出す
    RUIJINEN-37 スノーモンキーテーマが有効化されてないと警告を出すようにする

--- inc/is-snow-monkey-theme.php
+++ inc/is-snow-monkey-theme.php
@@ -10,5 +10,14 @@
  */
 $theme = wp_get_theme( get_template() );
 if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->template ) {
+	function add_alert_need_theme_snow_monkey() {
+		?>
+<div class="error">
+	<p><?php esc_html_e( '[RUI-JIN-EN Pattern Library] This Plugin needs the premium theme Snow Monkey.', 'rui-jin-en-pattern' ); ?></p>
+</div>
+		<?php
+	};
+	add_action( 'admin_notices', 'add_alert_need_theme_snow_monkey' );
+
 	return;
 }
--- rui-jin-en-pattern.php
+++ rui-jin-en-pattern.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: 類人猿パターンライブラリ（β版）
  * Description: Snow Monkeyサイトをより素敵にするブロックパターンが入っています
- * Version: 0.1.0
+ * Version: 0.1.0beta
  *
  * @package rui-jin-en-pattern
  * @author mgn
@@ -14,6 +14,14 @@
  */
 define( 'RJE_PLUGIN_URL', plugins_url( '', __FILE__ ) ); // このプラグインのURL
 define( 'RJE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) ); // このプラグインのパス
+
+/**
+ * テキストドメインを宣言
+ */
+function rje_pattern_load_textdomain() {
+	load_plugin_textdomain( 'rui-jin-en-pattern', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'rje_pattern_load_textdomain' );
 
 /**
  * inc ファイルの読み込み
