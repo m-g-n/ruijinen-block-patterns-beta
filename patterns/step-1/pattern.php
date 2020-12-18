fcd0b44b6539ce74d08c53ddcaeddcdce317599b
rina <rina@megane.in>
2020-12-18 14:16:29 +0900

    パターン追加

---
+++ patterns/step-1/pattern.php
@@ -0,0 +1,23 @@
+<!-- wp:snow-monkey-blocks/step -->
+<div class="wp-block-snow-monkey-blocks-step smb-step is-style-RJE-step-1"><div class="smb-step__body"><!-- wp:snow-monkey-blocks/step--item--free -->
+<div class="wp-block-snow-monkey-blocks-step--item--free smb-step__item"><div class="smb-step__item__title"><div class="smb-step__item__number"></div><span>ステップブロックを選びます</span></div><div class="smb-step__item__body"><div class="smb-step__item__summary"><!-- wp:paragraph -->
+<p>ステップブロックはSnow Monkey Blocksに入っています。<br>選んで編集画面に挿入しましょう。</p>
+<!-- /wp:paragraph --></div></div></div>
+<!-- /wp:snow-monkey-blocks/step--item--free -->
+
+<!-- wp:snow-monkey-blocks/step--item--free -->
+<div class="wp-block-snow-monkey-blocks-step--item--free smb-step__item"><div class="smb-step__item__title"><div class="smb-step__item__number"></div><span>ブロックに内容を入力しましょう</span></div><div class="smb-step__item__body"><div class="smb-step__item__summary"><!-- wp:paragraph -->
+<p>ステップブロックの中には、テキスト（段落）の他、画像やその他のブロックを入れることができます。<br>例えばカラムブロックの中に画像を並べるみたいなリッチなブロックも入ります。</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery columns-3 is-cropped"><ul class="blocks-gallery-grid"><li class="blocks-gallery-item"><figure><img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/step-sample-1.jpg" alt="流れ イメージ1" data-full-url="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/step-sample-1.jpg"/></figure></li><li class="blocks-gallery-item"><figure><img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/step-sample-2.jpg" alt="流れ イメージ2" data-full-url="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/step-sample-2.jpg"/></figure></li><li class="blocks-gallery-item"><figure><img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/step-sample-3.jpg" alt="流れ イメージ3" data-full-url="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/step-sample-3.jpg"/></figure></li></ul></figure>
+<!-- /wp:gallery --></div></div></div>
+<!-- /wp:snow-monkey-blocks/step--item--free -->
+
+<!-- wp:snow-monkey-blocks/step--item--free -->
+<div class="wp-block-snow-monkey-blocks-step--item--free smb-step__item"><div class="smb-step__item__title"><div class="smb-step__item__number"></div><span>さあ、公開しましょう</span></div><div class="smb-step__item__body"><div class="smb-step__item__summary"><!-- wp:paragraph -->
+<p>ブロックにコンテンツを入力したら、思い切って公開しましょう！</p>
+<!-- /wp:paragraph --></div></div></div>
+<!-- /wp:snow-monkey-blocks/step--item--free --></div></div>
+<!-- /wp:snow-monkey-blocks/step -->
