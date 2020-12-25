a0d9c9ca591aee7c006bf35805265766e4897143
lunaluna <web@lunalunadesign.com>
2020-12-25 18:54:27 +0900

    0.1.0 release

--- ruijinen-block-patterns-beta.php
+++ ruijinen-block-patterns-beta.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: 類人猿ブロックパターン（ベータ）
  * Description: Snow Monkeyサイトをより素敵にするブロックパターンが入っています
- * Version: 0.0.8
+ * Version: 0.1.0
  *
  * @package ruijinen-block-patterns-beta
  * @author mgn
