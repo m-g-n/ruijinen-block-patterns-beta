83086e866b14fb6d0e0bf60273ccf05685e146bb
rina <rina@megane.in>
2021-01-18 12:44:30 +0900

    パターン追加

---
+++ patterns/141-cta-1/pattern.php
@@ -0,0 +1,29 @@
+<!-- wp:snow-monkey-blocks/section-with-bgimage {"wrapperTagName":"section","lgImageMediaType":"image","contentsAlignment":"center","maskColor":"#000","maskOpacity":0.5,"textColor":"#fff","align":"full","className":"is-style-RJE-section-cta-1"} -->
+<section class="wp-block-snow-monkey-blocks-section-with-bgimage alignfull smb-section smb-section-with-bgimage smb-section-with-bgimage--center smb-section-with-bgimage--fit is-style-RJE-section-cta-1" style="color:#fff"><div class="smb-section-with-bgimage__bgimage smb-section-with-bgimage__bgimage--lg"><div class="smb-section-with-bgimage__mask" style="background-color:#000"></div><img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) ); ?>images/cta-sample-1.jpg" alt="サンプル画像" class="wp-image-0" style="opacity:0.5"/></div><div class="c-container"><h2 class="smb-section__title">ご相談は、お気軽にお問い合わせください。</h2><div class="smb-section__body"><!-- wp:snow-monkey-blocks/buttons -->
+<div class="wp-block-snow-monkey-blocks-buttons smb-buttons"><!-- wp:snow-monkey-blocks/btn {"borderRadius":0,"backgroundColor":"#fff","textColor":"#fff","align":"center","className":"is-style-ghost"} -->
+<div class="wp-block-snow-monkey-blocks-btn aligncenter smb-btn-wrapper is-style-ghost"><a class="smb-btn" href="" style="background-color:#fff;border-radius:0px;border-color:#fff"><span class="smb-btn__label" style="color:#fff">お問い合わせ</span></a></div>
+<!-- /wp:snow-monkey-blocks/btn --></div>
+<!-- /wp:snow-monkey-blocks/buttons -->
+
+<!-- wp:separator {"className":"is-style-wide"} -->
+<hr class="wp-block-separator is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"level":4} -->
+<h4>メール・お電話でのお問い合わせはこちら</h4>
+<!-- /wp:heading -->
+
+<!-- wp:snow-monkey-blocks/container {"className":"is-style-RJE-contact-1"} -->
+<div class="wp-block-snow-monkey-blocks-container smb-container c-container is-style-RJE-contact-1"><div class="smb-container__body"><!-- wp:paragraph {"align":"left","className":"is-style-RJE-contact-mail-1"} -->
+<p class="has-text-align-left is-style-RJE-contact-mail-1">info@xxxxx.com</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","className":"is-style-RJE-contact-tel-1"} -->
+<p class="has-text-align-left is-style-RJE-contact-tel-1">0000-000-000</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:snow-monkey-blocks/container -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":12}}} -->
+<p style="font-size:12px">受付時間 : 土・日・祝日を除く 10：00〜18：00</p>
+<!-- /wp:paragraph --></div></div></section>
+<!-- /wp:snow-monkey-blocks/section-with-bgimage -->
